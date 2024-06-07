1f19b4cd69f968cddd80d64634570eb0f4321063
KODOKYUN <hwasungnj3@gmail.com>
2024-06-07 16:40:47 +0900

    240607

--- src/main/java/com/busanit501/boot501/controller/BoardController.java
+++ src/main/java/com/busanit501/boot501/controller/BoardController.java
@@ -4,12 +4,16 @@ import com.busanit501.boot501.dto.BoardDTO;
 import com.busanit501.boot501.dto.PageRequestDTO;
 import com.busanit501.boot501.dto.PageResponseDTO;
 import com.busanit501.boot501.service.BoardService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 // 화면, 데이터 같이 전달.
 @Controller
@@ -24,6 +28,23 @@ public class BoardController {
     public void list(PageRequestDTO pageRequestDTO, Model model) {
         PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
         model.addAttribute("responseDTO", responseDTO);
+    }
 
+    @GetMapping("/register")
+    public void registerForm(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("board register 등록 화면 Get  테스트 콘솔");
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        if(bindingResult.hasErrors()) {
+            log.info("register 중 오류 발생");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+        log.info("화면에서 입력 받은 내용 확인" + boardDTO);
+        Long bno=boardService.register(boardDTO);
+        redirectAttributes.addFlashAttribute("result",bno);
+        return "redirect:/board/list";
     }
 }
